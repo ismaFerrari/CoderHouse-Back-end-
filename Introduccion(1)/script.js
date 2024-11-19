40723d5dc2894401ff5eb76966ab3e7e08c08ab4
ismaFerrari <ismaelferrari97@gmail.com>
2024-11-19 17:07:18 -0300

    Primeros cambios en el repositorio.

---
+++ Introduccion(1)/script.js
@@ -0,0 +1,46 @@
+// Actividad: Configuración del Entorno y Primer Script en JavaScript
+var Texto = "Hola, Mundo"
+    console.log(Texto)
+
+// Actividad: Uso de Variables y Tipos de Datos
+    let number = 12;
+    console.log(number); 
+    
+    let string = "Hola Mundo";
+    console.log(string); 
+    
+    let bool = true; // o false
+    console.log(bool); 
+
+
+// Actividad: Operaciones Matemáticas Básicas
+var a = 1;
+var b = 2;
+console.log( a + b);
+
+
+//Actividad: Interacción con el Usuario
+
+//Utiliza prompt para pedirle al usuario que ingrese su nombre.
+let nombre = prompt("Por favor, ingresa tu nombre:");
+console.log("Hola, " + nombre + "! Bienvenido/a.");
+
+
+//Usa alert para mostrar un mensaje de bienvenida personalizado que incluya el nombre ingresado.
+
+let name = prompt("Por favor, ingresa tu nombre:");
+alert(`¡Bienvenido/a, ${name}! Nos alegra que estés aquí.`);
+
+//A continuación se presenta un fragmento de código que contiene varios errores. 
+//Encuentra y corrígelos para que funcione correctamente. 
+//jsx let nombre = "Ana; const edad = 25; let mensaje = "Hola, " + nombre + "! Tienes " + edad " años."; consol.log(mensaje);
+
+let nombre1 = "Ana"; 
+const edad = 25;
+let mensaje =  "Hola, " + nombre + "! Tienes " + edad + " años.";
+console.log(mensaje);
+
+
+
+
+
---
+++ Nivelacion(0)/Script.js
@@ -0,0 +1,8 @@
+let number = 12;
+console.log(number); 
+
+let string = "Hola Mundo";
+console.log(string); 
+
+let bool = true; // o false
+console.log(bool); 
